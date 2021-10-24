559e15c24dee9872bf44e3fec0da37a069fce6f2
ilia <salomatin.ilia@gmail.com>
2021-10-24 14:51:21 +0300

    first

--- find_in_base.py
+++ find_in_base.py
@@ -1,28 +1,32 @@
 import pandas as pd
 
-WORD = 'fluently'
+WORDS = ["foreign", "obvious", "surprising", "try on", "these", "trousers", "spend on"]
 N = 3
+
+
 
 base = pd.read_csv('111.csv')
 
 
-for i in range(N + 1):
-    if i != 0:
-        try:
-            print(*base.loc[base['word'] == WORD[:-i]].iloc[0], sep=',')
-            break
-        except IndexError:
-            None
-    else:
-        try:
-            print(*base.loc[base['word'] == WORD].iloc[0], sep=',')
-            break
-        except IndexError:
-            None
-    if i == N:
-        print('слово не найдено')
+def get_str(word):
+    for i in range(N + 1):
+        if i != 0:
+            try:
+                print(*base.loc[base['word'] == word[:-i]].iloc[0], sep=',')
+                break
+            except IndexError:
+                None
+        else:
+            try:
+                print(*base.loc[base['word'] == word].iloc[0], sep=',')
+                break
+            except IndexError:
+                None
+        if i == N:
+            print('слово не найдено')
 
-
+for word in WORDS:
+    get_str(word)
 
 #
 # try:
