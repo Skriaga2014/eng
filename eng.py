a5cc6ec278a6db9ef02edbef422edc84303e98f1
ilia <salomatin.ilia@gmail.com>
2021-10-24 15:42:19 +0300

    2

--- eng.py
+++ eng.py
@@ -3,7 +3,7 @@ import datetime as dt
 
 
 VARS_NUM = 5
-TYPE = 'en' # 'ru_en'/'en_ru'/'ru'/'en'
+TYPE = 'en'  # 'ru_en'/'en_ru'/'ru'/'en'
 BASE_FILE = 'my_dict.csv'
 BASE_FILE_RESERVE = 'my_dict_r.csv'
 
@@ -11,7 +11,6 @@ BASE_FILE_RESERVE = 'my_dict_r.csv'
 # load dictionary
 def get_base(file, file_r, type_=TYPE, vars_num=VARS_NUM):
     # проверка, не затесалось ли nan в shows и/или result
-    #file = '1231.csv'
     try:
         base = pd.read_csv(file, dtype={'shows': int, 'result': int})
     except ValueError:
@@ -21,14 +20,20 @@ def get_base(file, file_r, type_=TYPE, vars_num=VARS_NUM):
         base = pd.read_csv(file_r).fillna(0)
         base = base.astype({'shows': 'int32', 'result': int})
 
-    if len(base) < vars_num:
-        vars_num = len(base)
+    len_base = len(base)
+
+    if len_base < vars_num:
+        vars_num = len_base
 
     if type_ in ['en', 'ru']:
-        base = base.sort_values('result').head(vars_num)
-    #base = base.set_index(base['id'])
-    print(base)
-    return base
+        sorted_base = base.sort_values('result')
+        base = sorted_base.head(vars_num)
+        base2 = sorted_base.tail(len_base - vars_num)
+        print(base, '\n', base2)
+    else:
+        base2 = None
+
+    return base, base2
 
 
 # create task and variants
@@ -56,6 +61,9 @@ def get_task_vars(base, VARS_NUM, TYPE):
             exit('ERROR: Too short len of base')
         task = base.sample(VARS_NUM)
         variants = None
+
+    print('task:', task)
+    print('variants:', variants)
 
     show_question(task, variants, TYPE)
     return task, variants
@@ -94,20 +102,30 @@ def to_log(typee, task, variants, answer, check, file):
         f.write(line)
 
 
-def base_update(base, task, check, base_file, base_file_reserve):
+def base_update(base, base2, task, check, base_file, base_file_reserve):
+    # print('+++', base)
+    # print(task)
+    # print(base['id'])
+    # print('-----', task['id'][0])
+
+    print(base.loc[base['id'] == task['id'][0], ['shows']])
     base.loc[base['id'] == task['id'][0], ['shows']] += 1
     if check:
         base.loc[base['id'] == task['id'][0], ['result']] += 1
-    base.to_csv(base_file, index=0)
-    base.to_csv(base_file_reserve, index=0)
+    if base2 != 0:
+        base.join(base2)
+    print(base)
+    #base.to_csv(base_file, index=0)
+    #base.to_csv(base_file_reserve, index=0)
+
 
 # main
 def go():
-    base = get_base(BASE_FILE, BASE_FILE_RESERVE)
+    base, base2 = get_base(BASE_FILE, BASE_FILE_RESERVE) # base2 may be not exists (== 0)
     task, variants = get_task_vars(base, VARS_NUM, TYPE)
     answer = input('answer: ')
     check = get_check(answer, task, TYPE)
-    base_update(base, task, check, BASE_FILE, BASE_FILE_RESERVE)
+    base_update(base, base2, task, check, BASE_FILE, BASE_FILE_RESERVE)
     print('right' if check else f'wrong ({task["translate"].tolist()[0]})\n')
     to_log(TYPE, task, variants, answer, check, 'log.csv')
 
